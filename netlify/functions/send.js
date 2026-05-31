b8513fc5bd3ea84cf7d1e24bff1fbbea9a7a4d06
Anton Boyarshinov <anton.unixxx@gmail.com>
2026-05-31 12:46:23 +0300

    Добавлена базовая валидация

--- netlify/functions/send.js
+++ netlify/functions/send.js
@@ -7,10 +7,24 @@ export const handler = async (event) => {
 
     try {
         const {name, phone, email, message} = JSON.parse(event.body);
+        const cleanName = name?.trim();
+        const cleanPhone = phone?.trim();
+        const cleanEmail = email?.trim();
+        const cleanMessage = message?.trim();
 
 
-        if (!name || !phone || !email || !message) {
+        if (!cleanName || !cleanPhone || !cleanEmail || !cleanMessage) {
             return {statusCode: 400, body: JSON.stringify({error: "Все поля должны быть заполнены"})};
+        }
+        if (cleanName.length > 50 || cleanMessage.length > 2000) {
+            return { statusCode: 400, body: JSON.stringify({ error: "Превышена допустимая длина полей" }) };
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(cleanEmail)) {
+            return { statusCode: 400, body: JSON.stringify({ error: "Некорректный формат Email" }) };
+        }
+        if (cleanPhone.length < 18) {
+            return { statusCode: 400, body: JSON.stringify({ error: "Номер телефона заполнен не полностью" }) };
         }
 
         const transporter = nodemailer.createTransport({
@@ -26,16 +40,16 @@ export const handler = async (event) => {
 
         const emailText = `Сообщение с визитки\n\n` +
             `Данные отправителя:\n` +
-            `• Имя: ${name}\n` +
-            `• Телефон: ${phone}\n` +
-            `• Email: ${email}\n\n` +
-            `Комментарий:\n${message}`;
+            `• Имя: ${cleanName}\n` +
+            `• Телефон: ${cleanPhone}\n` +
+            `• Email: ${cleanEmail}\n\n` +
+            `Комментарий:\n${cleanMessage}`;
 
         await transporter.sendMail({
             from: `"Сайт-Визитка" <${process.env.EMAIL_USER}>`,
             to: process.env.EMAIL_USER,
-            cc: email,
-            subject: `Сообщение от ${name} (${phone})`,
+            cc: cleanEmail,
+            subject: `Сообщение от ${cleanName} (${cleanPhone})`,
             text: emailText,
         });
 
